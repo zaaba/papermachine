46b6769c08a6cf879ff7993ab9e8a5a807697457
zaaba <zaaba.ahmad@uitm.edu.my>
2025-04-17 12:57:19 +0800

    Fix: update CORS for GitHub Pages

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500"],
+    allow_origins=["https://zaaba.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
